6e9f29f193d6a60e83e32fd44fff8698079cc939
I-dingan <ashwinsatheesh1998@gmail.com>
2022-07-26 00:48:01 +0530

    final

--- pages/search.js
+++ pages/search.js
@@ -7,7 +7,7 @@ import {useRouter} from "next/router";
 import SearchResults from "./SearchResults";
 
 function Search({ results }) {
-  console.log(results);
+
   const router=useRouter()
   return (
     <div>
@@ -25,14 +25,14 @@ function Search({ results }) {
 export default Search;
 
 export async function getServerSideProps(context) {
-  const useDummyData = true;
+  const useDummyData = false;
   
   const startIndex =context.query.start || "0"
   const data = useDummyData
     ? Response
     : await fetch(
-        `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${context.query.term}&start=${startIndex}`
-      ).then((response) => Response.json());
+        `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${context.query.term}&start=${startIndex}`
+      ).then((response) => response.json());
   // after ssr..pass result to client
   return {
     props: {
