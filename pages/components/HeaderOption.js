234c556d352ae201c1ee19159d89ddf3b54452f6
I-dingan <ashwinsatheesh1998@gmail.com>
2022-07-26 13:39:54 +0530

    final

--- pages/components/HeaderOption.js
+++ pages/components/HeaderOption.js
@@ -6,7 +6,7 @@ function HeaderOption({ Icon, title, selected }) {
      hover:text-blue-500 hover:border-blue-500 pb-3 cursor-pointer
       ${(selected ? "text-blue-500 border-blue-500":'')}`}
     >
-      <Icon className="h-4" />
+      {Icon? <Icon className='h-4'/>:null}
       <p className="hidden sm:inline-flex">{title}</p>
     </div>
   );
